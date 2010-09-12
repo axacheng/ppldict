c62973d4601a7205b32df2957cee48eb5ebe5a00
axa.cheng <axa.cheng@5a5fc1f5-5569-7fc3-b921-f0196c2df41a>
2010-09-12 17:35:43 +0000

    1.) /add handler is completed 2.) /add jquery is completed 3.) /add html form submit is completed

---
+++ ppldict/src/stylesheet/add.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+	
+	//First hide form on add_form, when user clicking 'Add' it will show up
+	$(".toggle").click(function(){
+    $("#add_main").slideToggle("fast");
+    }) 
+    $("#add_main").hide()
+    
+  // Animation, for submitting form
+  $('#add_form').submit(function(){
+			var form = $(this),
+			formData = form.serialize(),
+			formMethod = form.attr('method'),
+			responseText = $('#submit_result') 
+			responseText.hide()
+			            .addClass('waiting')
+									.text('Please wait...')
+									.fadeIn(200);
+    $.ajax({
+				url: "/add",
+				type: formMethod,
+				data: formData,
+				success: function(data){
+					var responsData = $.parseJSON(data),
+					    responseData_class = '';
+					switch(responsData.status){
+						case 'error':
+						  responseData_class = 'response_error';
+						break;
+						case 'success':
+						  responseData_class = 'response_success';
+						break;				
+					}
+					
+					responseText.fadeOut(200, function(){
+						$(this).removeClass('waiting')
+						       .addClass(responseData_class)
+									 .text(responsData.message)
+									 .fadeIn(200,function(){
+									     setTimeout(function(){
+									     responseText.fadeOut(200,function(){
+									         $(this).removeClass(responseData_class);
+										 });
+										 }, 3000);
+					$("#add_main").slideToggle("slow");
+									 });
+					
+					});					
+//					alert(responsData.message);
+				}
+			});
+
+			return false;
+    })
+
+})
